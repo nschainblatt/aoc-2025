506c069e724ab50cae2620adc84c465c4c3d6d37
nate <nschainblatt@gmail.com>
2025-12-06 21:00:35 -0500

    complete day 2 part 2

--- day2/ids.scm
+++ day2/ids.scm
@@ -23,17 +23,54 @@
 (define (count-through-range start end sum)
   (if (> start end)
     sum
-    (let ((start-as-string (number->string start)))
-      (if (= (remainder (string-length start-as-string) 2) 0)
-        (let* ((left-half (string-slice start-as-string 0 (quotient (string-length start-as-string) 2)))
-               (right-half (string-slice start-as-string (quotient (string-length start-as-string) 2)))
-               (new-sum (if (equal? left-half right-half) (+ sum start) sum)))
-          (count-through-range (+ start 1) end new-sum))
-        (count-through-range (+ start 1) end sum)))))
+    (let* ((start-as-string (number->string start))
+           (new-sum (if (is-id-invalid? start-as-string) (+ sum start) sum)))
+      (count-through-range (+ start 1) end new-sum))))
 
-(define (println x)
+;; Take the id, inspect sequence by sequence, starting with the first digit, increasing in size over time if no matches found so far.
+(define (is-id-invalid? id-string)
+  (define (iter seq rest)
+          ;; If the rest of the string is empty, that means the seq is the whole string, thus no two matches exist.
+    (cond ((string-empty? rest) #f)
+          ((is-rest-repeating-seq? seq rest) #t)
+          ;; Invalid condition not met yet, increase the size of the seq by one, check for matches again.
+          (else (iter (string-append seq (substring rest 0 1)) (substring rest 1)))))
+
+  ;; Return true if rest only contains repeats of seq, otherwise false.
+  ;; Use two pointers to track separate positions in seq and rest.
+  ;; As soon as a seq match is found in rest, the seq-ptr is reset to allow further matches to be found.
+  (define (is-rest-repeating-seq? seq rest)
+
+    (define (iter seq-ptr rest-ptr)
+            ;; We've reached the end of the comparison, everything matched. seq-ptr must also be oob to prove that the last digit
+            ;; of the id was apart of the last digit of the last match.
+      (cond ((and (out-of-bounds? rest rest-ptr) (out-of-bounds? seq seq-ptr)) #t)
+            ;; We've reached the end, seq isn't out of bounds, meaning it didn't finish it's last match before rest ended.
+            ((out-of-bounds? rest rest-ptr) #f)
+            ;; End of the current match, check the next one.
+            ((out-of-bounds? seq seq-ptr) (iter 0 rest-ptr))
+            ;; Check the current characters ensuring they are matching.
+            (else
+              (let ((seq-chr (string-ref seq seq-ptr))
+                    (rest-chr (string-ref rest rest-ptr)))
+                (if (equal? seq-chr rest-chr)
+                  (iter (inc seq-ptr) (inc rest-ptr))
+                  #f)))))
+    (iter 0 0))
+
+  (iter (substring id-string 0 1) (substring id-string 1)))
+
+(define (println . args)
   (newline)
-  (display x))
+  (for-each (lambda (x) (display x) (display " ")) args))
 
+(define (inc x)
+  (+ x 1))
 
-(println (call-with-input-file "input" read-file))
+(define (out-of-bounds? str index)
+  (> index (- (string-length str) 1)))
+
+(define (string-empty? str)
+  (= (string-length str) 0))
+
+(println (call-with-input-file "example-input" read-file))
